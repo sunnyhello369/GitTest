bdb50e904779b59fc508866da4377e9009b41a5c
sunnyhello369 <sunnyhello369@gmail.com>
2022-11-24 12:04:53 +0800

    dddd

---
+++ src/main/java/test02.java
@@ -0,0 +1,10 @@
+/**
+ * @projectName: GitTest
+ * @className: test02
+ * @author: John Dong
+ * @description: TODO
+ * @date: 2022/11/24
+ * @version: 1.0
+ */
+public class test02 {
+}
---
+++ src/main/java/test03.java
@@ -0,0 +1,10 @@
+package PACKAGE_NAME;/**
+ * @projectName:    GitTest 
+ * @className:      test03
+ * @author:     John Dong
+ * @description:  TODO  
+ * @date:    2022/11/24
+ * @version:    1.0
+ */ 
+public class test03 {
+}
