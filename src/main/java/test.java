09b032f8afc2f1566ab12f553b8bb5383866b670
sunnyhello369 <sunnyhello369@gmail.com>
2022-11-24 11:48:49 +0800

    test01

---
+++ src/main/java/test.java
@@ -0,0 +1,13 @@
+/**
+ * @projectName: GitTest
+ * @className: test
+ * @author: John Dong
+ * @description: TODO
+ * @date: 2022/11/24
+ * @version: 1.0
+ */
+public class test {
+    public static void main(String[] args) {
+        System.out.println("Hello");
+    }
+}
