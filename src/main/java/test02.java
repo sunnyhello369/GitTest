6f5ccc77f44d96f4ea75c82b79f10b04ebbaa4b8
sunnyhello369 <sunnyhello369@gmail.com>
2022-11-24 12:29:26 +0800

    nnn

---
+++ src/main/java/test02.java
@@ -0,0 +1,11 @@
+/**
+ * @projectName: GitTest
+ * @className: test02
+ * @author: John Dong
+ * @description: TODO
+ * @date: 2022/11/24
+ * @version: 1.0
+ */
+public class test02 {
+    short aShort;
+}
